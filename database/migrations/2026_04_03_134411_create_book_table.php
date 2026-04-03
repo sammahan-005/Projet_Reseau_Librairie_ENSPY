3f0855f9b0001d9a02dd2ea5f8f41857aa484850
Ivana SADEU <ivanasadeu@gmail.com>
2026-04-03 15:38:25 +0100

    database migrations

---
+++ database/migrations/2026_04_03_134411_create_book_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('auteur');
+            $table->enum('categorie', ['roman', 'poême', 'théâtre', 'science-fiction', 'roman-policier', 'bande-dessinée'])->default('roman');
+            $table->date('année de publication');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_book');
+    }
+};
---
+++ database/migrations/2026_04_03_142056_create_paiements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('paiements', function (Blueprint $table) {
+            $table->id();
+            $table->string('moyen de paiement');
+            $table->integer('montant');
+            $table->date('date de paiement');
+            $table->enum('statut', ['en attente', 'effectué', 'échoué'])->default('en attente');
+            $table->foreignId('subscription_id')->constrained('subscriptions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('paiements');
+    }
+};
